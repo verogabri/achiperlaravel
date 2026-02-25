27c3ced8cdaf4804864bce857f7e1325fffaaf09
verogabri <gabriele.peressutti@gmail.com>
2026-02-25 21:44:46 +0100

    first commit 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>
+        Home Feed
+    </x-slot:title>
+
+    <div class="container">
+        <h1 class="text-3xl font-bold mb-4">Welcome to Chiper!</h1>
+        <p class="text-lg text-gray-700">Your one-stop solution for all your needs.</p>
+    </div>
+    
+</x-layout>
